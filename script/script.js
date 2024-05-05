badcb544f496672e486a82bb46614ee77a72e48c
Matheus Brito <kiichyy7@gmail.com>
2024-05-04 21:58:27 -0300

    alterações

--- script/script.js
+++ script/script.js
@@ -1,4 +1,19 @@
-const camposDoFormulario = document.querySelectorAll("[required]");
+const camposDoFormulario = document.querySelectorAll('[required]');
+const formulario = document.querySelector('[data-formulario]');
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const listaRespostas = {
+        'nome': e.target.elements['nome'].value,
+        'email': e.target.elements['email'].value,
+        'assunto': e.target.elements['assunto'].value,
+        'mensagem': e.target.elements['mensagem'].value,
+    }
+
+    localStorage.setItem('contato', JSON.stringify(listaRespostas));
+
+})
 
 // console.log(camposDoFormulario);
 const tiposDeErro = [
@@ -45,16 +60,45 @@ function verificaCampo(campo) {
             console.log(mensagem)
         }
     })
+
     const mensagemErro = campo.parentNode.querySelector('#mensagemErro');
     const validadorDeInput = campo.checkValidity();
+    const iconeErro = '<i class="fa-solid fa-circle-exclamation"></i>';
 
-    mensagemErro.classList.add('ativo', 'animate__animated', 'animate__fadeIn');
-    let iconeErro = '<i class="fa-solid fa-circle-exclamation"></i>';
-
+    // mensagemErro.classList.add('ativo', 'animate__animated', 'animate__fadeIn');
+    // --------------------------------- //
+    
     if(!validadorDeInput) {
-        mensagemErro.innerHTML = iconeErro + ' ' + mensagem;
+        mensagemErro.innerHTML = `${iconeErro} ${mensagem}`;
+        mensagemErro.setAttribute('mostrar', '')
     } else {
-        mensagemErro.textContent = "";
-        mensagemErro.classList.remove('ativo');
+        mensagemErro.setAttribute('esconder', '');
+        btnSubmit.classList.remove('btn-disabled')
+
+
+        mensagemErro.addEventListener('animationend', () => {
+            mensagemErro.removeAttribute('esconder');
+            mensagemErro.removeAttribute('mostrar');
+        }, {once: true})
     }
+    
+    // --------------------------------- //
+    // if(!validadorDeInput) {
+    //     mensagemErro.setAttribute('mostrar', '')
+    //     mensagemErro.innerHTML = `${iconeErro} ${mensagem}`
+    // } else {
+    //     mensagemErro.setAttribute('esconder', '');
+    //     mensagemErro.textContent = "";
+        
+
+    //     mensagemErro.addEventListener('animationend', () => {
+    //         mensagemErro.removeAttribute('esconder', '')
+    //         mensagemErro.removeAttribute('mostrar', '')
+    //         // mensagemErro.textContent = "";
+
+            
+    //     }, {once: true})
+    //     // mensagemErro.classList.add('animate__animated', 'animate__fadeOut')
+    //     // mensagemErro.classList.remove('ativo');
+    // }
 }
